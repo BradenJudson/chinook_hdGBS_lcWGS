03e14081fe5b1197cea7b4454e10bb494e212fc5
Judson <Braden.Judson@dfo-mpo.gc.ca>
2023-12-08 09:30:49 -0800

    add map script and shapefiles

---
+++ map/chinook_map.R
@@ -0,0 +1,13 @@
+################################################################################
+#
+# Author: Braden J. Judson
+# Email: Braden.Judson@dfo-mpo.gc.ca // BradenJudson95@gmail.com
+# Date created: December 8, 2023
+# Date last modified: December 08, 2023
+# Script description: Creating a map of sample sites and conservation unit outlines
+# for British Columbian Chinook Salmon. 
+#
+################################################################################
+
+setwd("~/landscape_genetics/map")
+
